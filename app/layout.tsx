512362334053e4a25901186d412c21bee638a1c5
Maxwell Lo <lo.maxwell8240@gmail.com>
2026-01-06 12:26:08 -0800

    more seo, update nextjs vercel analytics import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import { lightTheme } from "@/src/theme/mui-theme";
 import { ThemeProvider } from "@mui/material";
 import { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -24,8 +25,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        id="structured-data"
+        type="application/ld+json"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            name: "Baking Ingredient Substitution Engine",
+            url: "https://your-domain.com",
+            description:
+              "Find smart ingredient substitutions and baking effects.",
+          }),
+        }}
+      />
       <head>
-      <meta name="google-site-verification" content="SfPj_WTSxLXjEqapUeg8vCbrsf663k4ISSuuEkhru2E" />
+        <meta name="google-site-verification" content="SfPj_WTSxLXjEqapUeg8vCbrsf663k4ISSuuEkhru2E" />
         <script type="application/ld+json" src="/schema/website.json" />
       </head>
       <body>
